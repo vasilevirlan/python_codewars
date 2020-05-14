435c3555d206c5b9f5afd764ee2ffc011d7b07b2
vasilevirlan <40796144+vasilevirlan@users.noreply.github.com>
2020-05-14 17:56:59 +0300

    Added 14.05.202 a simple sorting tuple function.
    From hexlet.io exercise. See docstring

---
+++ sorting_tuples.py
@@ -0,0 +1,12 @@
+def sort_pairs():
+    """Make a function that recieve a tuple with two integers
+        and return a sorted tuple in ascending maner if first
+        tuple member is greatest then second.
+
+        From hexlet.io Python Basics Lesspn nr.9 Tuples.
+    """
+
+    my_tuple = tuple(my_tuple)
+    my_list_from_tuple = list(my_tuple)
+    return tuple(sorted(my_list_from_tuple))
+
